cc8d077885f0aa6f1a27c6163403df214b3bcb2d
Pranavmc <prananvchougale17@gmail.com>
2025-12-05 16:40:50 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const http = require("http");
+const server = http.createServer((req, res) => {
+  res.end("CI/CD Deployment Successful!");
+});
+server.listen(3000, () => console.log("App running on 3000"));
